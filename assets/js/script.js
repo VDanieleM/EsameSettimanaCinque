d507baa24c97583284524d38869ef323b4108cf1
Your Name <vdanielem@gmail.com>
2023-11-27 17:52:01 +0100

    Effetto Fade M

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
+/* Effetto Fade Nav */
+
 window.onscroll = function() {
     let navbar = document.querySelector('nav');
     let myButton = document.querySelector('.btnGreen');
     let hover = document.querySelectorAll('nav div .btn');
   
-    if (window.pageYOffset > 390) {
+    if (window.pageYOffset > 390) { /* Anche se deprecato funziona */
       navbar.classList.add('scrolled');
       myButton.classList.add('scrolled');
       hover.forEach(hover => hover.classList.add('scrolled'));
@@ -13,3 +15,46 @@ window.onscroll = function() {
       hover.forEach(hover => hover.classList.remove('scrolled'));
     }
   };
+
+/*   Effetto fade M */
+
+  const fadeInDuration = 1000; 
+  const fadeOutDuration = 1000; 
+  const delayBetweenCycles = 2000;
+  
+  const textGroup = document.querySelector('g[aria-label]');
+  const textElements = Array.from(textGroup.children);
+  
+  function fadeIn(element, duration) {
+    element.style.transition = `opacity ${duration}ms`;
+    element.style.opacity = 1;
+  }
+  
+  function fadeOut(element, duration) {
+    element.style.transition = `opacity ${duration}ms`;
+    element.style.opacity = 0;
+  }
+  
+  function fadeCycle() {
+
+    const shuffledElements = textElements.sort(() => Math.random() - 0.5);
+  
+    shuffledElements.forEach((element, index) => {
+      setTimeout(() => {
+        fadeIn(element, fadeInDuration);
+      }, index * fadeInDuration); 
+    });
+  
+    setTimeout(() => {
+      shuffledElements.forEach((element, index) => {
+        setTimeout(() => {
+          fadeOut(element, fadeOutDuration);
+        }, index * fadeOutDuration);
+      });
+    }, fadeInDuration);
+  
+    setTimeout(fadeCycle, fadeInDuration + fadeOutDuration + delayBetweenCycles);
+  }
+  
+  fadeCycle();
+  
